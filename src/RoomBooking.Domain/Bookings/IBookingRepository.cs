0e042c745dac18ab0a2ba88726c6af4c7fcf448a
evgeniavolkova <evgeniafil04@gmail.com>
2026-09-11 14:25:34 +0500

    мелкая правка типов

--- src/RoomBooking.Domain/Bookings/IBookingRepository.cs
+++ src/RoomBooking.Domain/Bookings/IBookingRepository.cs
@@ -2,17 +2,17 @@ namespace RoomBooking.Domain.Bookings;
 
 public interface IBookingRepository
 {
-    Task<Bookings.Booking> AddAsync(Bookings.Booking booking, CancellationToken cancellationToken);
+    Task<Booking> AddAsync(Booking booking, CancellationToken cancellationToken);
 
-    Task DeleteAsync(int bookingId, CancellationToken cancellationToken);
+    Task DeleteAsync(Guid bookingId, CancellationToken cancellationToken);
 
-    Task<Bookings.Booking?> GetByIdAsync(int bookingId, CancellationToken cancellationToken);
+    Task<Booking?> GetByIdAsync(Guid bookingId, CancellationToken cancellationToken);
 
-    Task<List<Bookings.Booking>> GetAllAsync(CancellationToken cancellationToken);
+    Task<List<Booking>> GetAllAsync(CancellationToken cancellationToken);
 
-    Task<List<Bookings.Booking>> GetByRoomIdAsync(int roomId, CancellationToken cancellationToken);
+    Task<List<Booking>> GetByRoomIdAsync(int roomId, CancellationToken cancellationToken);
     
-    Task<List<Bookings.Booking>> GetByUserIdAsync(Guid userId, CancellationToken cancellationToken);
+    Task<List<Booking>> GetByUserIdAsync(Guid userId, CancellationToken cancellationToken);
 
-    Task UpdateAsync(Bookings.Booking booking, CancellationToken cancellationToken);
+    Task UpdateAsync(Booking booking, CancellationToken cancellationToken);
 }
