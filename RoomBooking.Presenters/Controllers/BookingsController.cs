ac9e46fff8f1cf681f72402866fa0d380f946f14
evgeniavolkova <evgeniafil04@gmail.com>
2026-09-11 15:15:09 +0500

    заполнила BookingsController.cs, до этого был только скелет

--- RoomBooking.Presenters/Controllers/BookingsController.cs
+++ RoomBooking.Presenters/Controllers/BookingsController.cs
@@ -1,48 +1,63 @@
 using Microsoft.AspNetCore.Mvc;
+using RoomBooking.Application.Bookings;
 using RoomBooking.Contracts.Bookings;
 
 namespace RoomBooking.Presenters.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class BookingsController : ControllerBase
 {
+    private readonly IBookingService _bookingService;
+
+    public BookingsController(IBookingService bookingService)
+    {
+        _bookingService = bookingService;
+    }
+
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] CreateBookingDto dto, CancellationToken cancellationToken)
     {
-        return Ok("Booking created");
+        var booking = await _bookingService.CreateAsync(dto, cancellationToken);
+        return CreatedAtAction(nameof(GetById), new { bookingId = booking.Id }, booking);
     }
 
     [HttpPost("{bookingId:guid}/cancel")]
     public async Task<IActionResult> CancelBooking([FromRoute] Guid bookingId, CancellationToken cancellationToken)
     {
-        return Ok("Booking cancelled");
+        await _bookingService.CancelBookingAsync(bookingId, cancellationToken);
+        return NoContent();
     }
 
     [HttpGet("{bookingId:guid}")]
     public async Task<IActionResult> GetById([FromRoute] Guid bookingId, CancellationToken cancellationToken)
     {
-        return Ok($"Booking {bookingId} has been retrieved");
+        var booking = await _bookingService.GetByIdAsync(bookingId, cancellationToken);
+        return Ok(booking);
     }
 
     [HttpGet]
     public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
     {
-        return Ok("List of all bookings");
+        var bookings = await _bookingService.GetAllAsync(cancellationToken);
+        return Ok(bookings);
     }
 
-    [HttpGet("{bookingId:guid}/bookings")]
+    [HttpGet("{roomId:int}/bookings")]
     public async Task<IActionResult> GetByRoomId([FromRoute] int roomId,
         CancellationToken cancellationToken)
     {
-        return Ok($"Bookings has been retrieved");
+        var bookings = await _bookingService.GetByRoomIdAsync(roomId, cancellationToken);
+        return Ok(bookings);
     }
 
     [HttpPatch("{bookingId:guid}/title")]
-    public async Task<IActionResult> ChangeBookingTitle([FromRoute] Guid bookingId, [FromBody] ChangeBookingTitleDto dto,
+    public async Task<IActionResult> ChangeBookingTitle([FromRoute] Guid bookingId,
+        [FromBody] ChangeBookingTitleDto dto,
         CancellationToken cancellationToken)
     {
-        return Ok("Title has changed");
+        await _bookingService.ChangeBookingTitleAsync(bookingId, dto, cancellationToken);
+        return NoContent();
     }
 
     [HttpPatch("{bookingId:guid}/description")]
@@ -50,7 +65,8 @@ public class BookingsController : ControllerBase
         [FromBody] ChangeBookingDescriptionDto dto,
         CancellationToken cancellationToken)
     {
-        return Ok("Description has changed");
+        await _bookingService.ChangeBookingDescriptionAsync(bookingId, dto, cancellationToken);
+        return NoContent();
     }
 
     [HttpPatch("{bookingId:guid}/schedule")]
@@ -58,13 +74,15 @@ public class BookingsController : ControllerBase
         [FromBody] ChangeBookingScheduleDto dto,
         CancellationToken cancellationToken)
     {
-        return Ok("Schedule has changed");
+        await _bookingService.ChangeBookingScheduleAsync(bookingId, dto, cancellationToken);
+        return NoContent();
     }
 
     [HttpPatch("{bookingId:guid}/room")]
     public async Task<IActionResult> ChangeBookingRoom([FromRoute] Guid bookingId, [FromBody] ChangeBookingRoomDto dto,
         CancellationToken cancellationToken)
     {
-        return Ok("Room has changed");
+        await _bookingService.ChangeBookingRoomAsync(bookingId, dto, cancellationToken);
+        return NoContent();
     }
 }
